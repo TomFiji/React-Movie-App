a4a7c1c0cd454eb63ea393be1380a1984fb50316
TomFiji <tomfijalkowski@yahoo.com>
2025-05-26 14:11:17 -0400

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import MovieCard from './components/MovieCard.jsx'
-import './App.css'
+import { useState } from 'react';
+import Home from "./pages/Home.jsx";
+import Favorites from './pages/Favorites.jsx';
+import {Routes, Route} from "react-router-dom"
+import './App.css';
+import NavBar from "./components/NavBar"
 
 function App() {
+
   return(
-    <>
-      <MovieCard movie={{title: "Tom's Film", release_date: "2025"}}/>
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
